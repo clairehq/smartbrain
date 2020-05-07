795baea2cd60dd5b2bcb717a53c4989dd4637c7c
clairehq <claireli.hq@gmail.com>
2020-05-06 19:43:20 -0700

    Updates

---
+++ precache-manifest.8fcfbae4c55eb6030c75c343f5b8740c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be9fe8a00bb19e467bfedf4ef2807276",
+    "url": "/smartbrain/index.html"
+  },
+  {
+    "revision": "2192928e3aa9beac7e4b",
+    "url": "/smartbrain/static/css/2.a6d8338d.chunk.css"
+  },
+  {
+    "revision": "c911751f0c21bf5e6a1e",
+    "url": "/smartbrain/static/css/main.a19b0a9a.chunk.css"
+  },
+  {
+    "revision": "2192928e3aa9beac7e4b",
+    "url": "/smartbrain/static/js/2.38427ce5.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/smartbrain/static/js/2.38427ce5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c911751f0c21bf5e6a1e",
+    "url": "/smartbrain/static/js/main.5d293ef7.chunk.js"
+  },
+  {
+    "revision": "e65e821cd885235ea5fc",
+    "url": "/smartbrain/static/js/runtime-main.54042368.js"
+  }
+]);
