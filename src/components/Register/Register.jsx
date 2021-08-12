cb1b909c14d10f05f1a0f0420ad56bf624e43d36
clairehq <claireli.hq@gmail.com>
2021-08-11 19:41:10 -0700

    update

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -48,7 +48,7 @@ class Register extends React.Component {
                     <main className="pa4 black-80">
                         <div className="measure">
                             <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-                                <legend className="f1 fw6 ph0 mh0">Register123</legend>
+                                <legend className="f1 fw6 ph0 mh0">Register</legend>
                                 <div className="mt3">
                                     <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
                                     <input onChange={this.onNameChange}className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="text" name="name"  id="name"/>
